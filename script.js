36517976603b02e1b3975265247818e9cad85ebf
unknown <jeannie.petah@gmail.com>
2020-07-16 15:05:37 +0300

    made some practise about scrolling to accept

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log('it works');
+
+const terms = document.querySelector('.terms-and-conditions');
+// const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+const obCallback = payload => {
+    //when the HR  is 100% on the screen, remove the disable at the
+   if (payload[0].intersectionRatio === 1) {
+       //make the accept button clickable!
+       button.disabled = false;
+       //stop observing the button
+       ob.unobserve(terms.lastElementChild);
+    }
+};
+//createthe observe and give it an action
+const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1,
+});
+//this will look for the HR element
+ob.observe(terms.lastElementChild);
